054c11571a52c1731340aac784e41ebe7612b5ef
xbaysal11 <baisalbek.daniiarov@gmail.com>
2023-06-21 00:49:51 +0600

    ADD components

---
+++ src/components/Plans.jsx
@@ -0,0 +1,6 @@
+
+export default function Plans() {
+  return (
+    <div>Plans</div>
+  )
+}
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import Plans from "./Plans";
+
+export { Header, Main, Footer, Plans };
