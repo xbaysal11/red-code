8cc96f3a4f00b68e0f65dc82742b45f9d8bd71ce
xbaysal11 <baisalbek.daniiarov@gmail.com>
2023-06-27 13:13:17 +0600

    ADD footer tags

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,40 @@
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="footer">
+      <div className="footer-body">
+        <div className="footer-body__left">
+          <div className="footer-brand">
+            <div className="footer-brand__logo"></div>
+            <div className="footer-brand__description">
+              A good design is not only aesthetically pleasing, but also
+              functional. It should be able to solve the problem
+            </div>
+          </div>
+        </div>
+        <div className="footer-body__right">
+          <div className="footer-links">
+            <div className="footer-links__title">Sections</div>
+            <div className="footer-links__items">
+              <div className="footer-links__item">Home</div>
+              <div className="footer-links__item">Home 2</div>
+              <div className="footer-links__item">Home 3</div>
+              <div className="footer-links__item">Home 4</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer-contacts">
+        <div className="footer-contacts__left">
+          <span>All Rights Reservd red-code.it 2023</span>
+        </div>
+        <div className="footer-contacts__right">
+          <div className="footer-contacts__list">
+            <div className="footer-contacts__item">Linkedin</div>
+            <div className="footer-contacts__item">Instagram</div>
+            <div className="footer-contacts__item">Phone</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
